0c965c49d4e1dcf46f48c822ecfdf8e3bdd53e01
Brandon <brandon@tendency.me>
2024-01-16 06:27:31 -0600

    ✨ Automatically boot services when booting the bot ✨ Implement a custom event loop ✨ Split booting into override-able methods ✨ Add support for registering services inside of the discord config 🔧 Remove unused bot description configuration

--- src/Laracord.php
+++ src/Laracord.php
@@ -5,19 +5,27 @@ namespace Laracord;
 use Discord\DiscordCommandClient as Discord;
 use Discord\WebSockets\Intents;
 use Exception;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Laracord\Commands\Command;
+use Laracord\Console\Commands\Command as ConsoleCommand;
 use Laracord\Logging\Logger;
-use LaravelZero\Framework\Commands\Command as LaravelCommand;
+use Laracord\Services\Service;
+use React\EventLoop\Loop;
 use ReflectionClass;
 
 class Laracord
 {
     /**
-     * The Command instance.
+     * The event loop.
+     */
+    protected $loop;
+
+    /**
+     * The console instance.
      *
-     * @var \LaravelZero\Framework\Commands\Command
+     * @var \Laracord\Console\Commands\Command
      */
     protected $console;
 
@@ -64,6 +72,11 @@ class Laracord
     protected array $commands = [];
 
     /**
+     * The bot services.
+     */
+    protected array $services = [];
+
+    /**
      * The registered bot commands.
      */
     protected array $registeredCommands = [];
@@ -71,7 +84,7 @@ class Laracord
     /**
      * Initialize the Discord Bot.
      */
-    public function __construct(LaravelCommand $console)
+    public function __construct(ConsoleCommand $console)
     {
         $this->console = $console;
     }
@@ -79,7 +92,7 @@ class Laracord
     /**
      * Make the Bot instance.
      */
-    public static function make(LaravelCommand $console): self
+    public static function make(ConsoleCommand $console): self
     {
         return new static($console);
     }
@@ -93,6 +106,20 @@ class Laracord
             throw new Exception('You must register at least one Discord bot command.');
         }
 
+        $this->beforeBoot();
+
+        $this->bootDiscord();
+
+        $this->getDiscord()->on('ready', fn () => $this->afterBoot());
+
+        $this->getDiscord()->run();
+    }
+
+    /**
+     * Boot the Discord client.
+     */
+    public function bootDiscord(): void
+    {
         $this->discord = new Discord([
             'token' => $this->getToken(),
             'prefix' => $this->getPrefix(),
@@ -102,7 +129,7 @@ class Laracord
         ]);
 
         foreach ($this->getCommands() as $command) {
-            $command = new $command($this);
+            $command = $command::make($this);
 
             $this->discord->registerCommand($command->getName(), fn ($message, $args) => $command->maybeHandle($message, $args), [
                 'cooldown' => $command->getCooldown() ?: 0,
@@ -114,25 +141,65 @@ class Laracord
 
             $this->registeredCommands[] = $command;
         }
+    }
 
-        $this->discord->on('ready', function ($discord) {
-            $commands = count($this->registeredCommands);
-            $commands = $commands === 1
-                ? "<fg=blue>{$commands}</> command"
-                : "<fg=blue>{$commands}</> commands";
+    /**
+     * Actions to run before booting the bot.
+     */
+    public function beforeBoot(): void
+    {
+        //
+    }
 
-            $this->console->log("Successfully booted <fg=blue>{$this->getName()}</> with {$commands}");
+    /**
+     * Actions to run after booting the bot.
+     */
+    public function afterBoot(): void
+    {
+        $commands = count($this->registeredCommands);
+        $commands = $commands === 1
+            ? "<fg=blue>{$commands}</> command"
+            : "<fg=blue>{$commands}</> commands";
 
-            $this->console->table(
-                ['<fg=blue>Command</>', '<fg=blue>Description</>'],
-                collect($this->registeredCommands)->map(fn ($command) => [
-                    $command->getSyntax(),
-                    $command->getDescription(),
-                ])->toArray()
-            );
-        });
+        $this->console->log("Successfully booted <fg=blue>{$this->getName()}</> with {$commands}");
 
-        $this->discord->run();
+        $this->bootServices();
+        $this->showCommands();
+    }
+
+    /**
+     * Handle the bot services.
+     */
+    public function bootServices(): void
+    {
+        foreach ($this->getServices() as $service) {
+            $service = $service::make($this);
+
+            try {
+                $service->boot();
+            } catch (Exception $e) {
+                $this->console->log("The <fg=red>{$service->getName()}</> service failed to boot.", 'error');
+                $this->console->log($e->getMessage(), 'error');
+
+                continue;
+            }
+
+            $this->console->log("The <fg=blue>{$service->getName()}</> service has been booted.");
+        }
+    }
+
+    /**
+     * Print the registered commands to console.
+     */
+    public function showCommands(): void
+    {
+        $this->console->table(
+            ['<fg=blue>Command</>', '<fg=blue>Description</>'],
+            collect($this->registeredCommands)->map(fn ($command) => [
+                $command->getSyntax(),
+                $command->getDescription(),
+            ])->toArray()
+        );
     }
 
     /**
@@ -201,12 +268,31 @@ class Laracord
         $defaultOptions = [
             'intents' => $this->getIntents(),
             'logger' => Logger::make($this->console),
+            'loop' => $this->getLoop(),
         ];
 
         return $this->options = [
             ...config('discord.options', []),
             ...$defaultOptions,
         ];
+    }
+
+    /**
+     * Get the bot services.
+     */
+    public function getServices(): array
+    {
+        if ($this->services) {
+            return $this->services;
+        }
+
+        $services = $this->extractClasses($this->getServicePath())
+            ->merge(config('discord.services', []))
+            ->unique()
+            ->filter(fn ($service) => is_subclass_of($service, Service::class) && ! (new ReflectionClass($service))->isAbstract())
+            ->all();
+
+        return $this->services = $services;
     }
 
     /**
@@ -218,7 +304,21 @@ class Laracord
             return $this->commands;
         }
 
-        $commands = collect(File::allFiles($this->getCommandPath()))
+        $commands = $this->extractClasses($this->getCommandPath())
+            ->merge(config('discord.commands', []))
+            ->unique()
+            ->filter(fn ($command) => is_subclass_of($command, Command::class) && ! (new ReflectionClass($command))->isAbstract())
+            ->all();
+
+        return $this->commands = $commands;
+    }
+
+    /**
+     * Extract classes from the provided application path.
+     */
+    protected function extractClasses(string $path): Collection
+    {
+        return collect(File::allFiles($path))
             ->map(function ($file) {
                 $relativePath = str_replace(
                     Str::finish(app_path(), DIRECTORY_SEPARATOR),
@@ -233,20 +333,14 @@ class Laracord
 
                 $className = Str::after($relativePath, $folders);
 
-                $command = app()->getNamespace().str_replace(
+                $class = app()->getNamespace().str_replace(
                     ['/', '.php'],
                     ['\\', ''],
                     $folders.$className
                 );
 
-                return $command;
-            })
-            ->merge(config('discord.commands', []))
-            ->unique()
-            ->filter(fn ($command) => is_subclass_of($command, Command::class) && ! (new ReflectionClass($command))->isAbstract())
-            ->all();
-
-        return $this->commands = $commands;
+                return $class;
+            });
     }
 
     /**
@@ -266,6 +360,26 @@ class Laracord
     }
 
     /**
+     * Get the path to the bot services.
+     */
+    public function getServicePath(): string
+    {
+        return app_path('Services');
+    }
+
+    /**
+     * Get the event loop.
+     */
+    public function getLoop()
+    {
+        if ($this->loop) {
+            return $this->loop;
+        }
+
+        return $this->loop = Loop::get();
+    }
+
+    /**
      * Get the Discord instance.
      */
     public function getDiscord(): Discord
@@ -276,7 +390,7 @@ class Laracord
     /**
      * Get the console instance.
      */
-    public function getConsole(): LaravelCommand
+    public function getConsole(): ConsoleCommand
     {
         return $this->console;
     }
