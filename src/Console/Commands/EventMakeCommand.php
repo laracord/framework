57a00b7247cd5a1e709094e32337d6967c24f3e3
Brandon <brandon@tendency.me>
2024-02-07 15:41:33 -0600

    🩹 Fix Windows support for the `make:event` command

--- src/Console/Commands/EventMakeCommand.php
+++ src/Console/Commands/EventMakeCommand.php
@@ -46,7 +46,7 @@ class EventMakeCommand extends GeneratorCommand
     {
         $stub = parent::replaceClass($stub, $name);
 
-        $events = collect(Event::getEvents());
+        $events = collect(Event::getEvents())->sort();
 
         $parameters = collect(
             File::json(__DIR__.'/../../../resources/data/events.json')
@@ -58,15 +58,15 @@ class EventMakeCommand extends GeneratorCommand
             $event = windows_os()
                 ? select(
                     'Select a Discord event to listen for',
-                    $events->pluck('name')->sort()->all(),
+                    $events->pluck('name')->keys()->all(),
                     scroll: 15,
                 )
                 : search(
                     label: 'Select a Discord event to listen for',
                     placeholder: 'Search for an event...',
                     options: fn (string $value) => strlen($value) > 0
-                        ? $events->pluck('name', 'key')->sort()->filter(fn ($name) => Str::contains(strtolower($name), strtolower($value)))->all()
-                        : $events->pluck('name', 'key')->sort()->all(),
+                        ? $events->pluck('name', 'key')->filter(fn ($name) => Str::contains(strtolower($name), strtolower($value)))->all()
+                        : $events->pluck('name', 'key')->all(),
                     scroll: 15,
                 );
 
