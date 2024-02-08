9fafc84055e0f6234c9d1cce837c62a67dc7de03
Brandon <brandon@tendency.me>
2024-02-08 03:52:50 -0600

    ✨ Implement a default Route service provider

---
+++ src/Http/Providers/RouteServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laracord\Http\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Define your route model bindings, pattern filters, and other route configuration.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+    }
+}
