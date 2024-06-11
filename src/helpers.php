3747749e8aaa14de8f616c1858958f159bcebef0
Brandon <brandon@tendency.me>
2024-06-11 13:17:19 -0500

    ✨ Add a `laracord_path()` helper to get the path relative to Laracord

---
+++ src/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! function_exists('laracord_path')) {
+    /**
+     * Retrieve the path to a file or directory relative to Laracord.
+     */
+    function laracord_path(string $path = '', bool $useBasePath = true): string
+    {
+        $basePath = $useBasePath ? '.laracord' : '';
+        $appPath = \Phar::running() ? pathinfo(\Phar::running(), PATHINFO_DIRNAME) : null;
+
+        return match ($appPath) {
+            null => base_path($basePath ? "{$basePath}/{$path}" : $path),
+            default => $appPath.($basePath ? "/{$basePath}" : '').($path ? "/{$path}" : '')
+        };
+    }
+}
