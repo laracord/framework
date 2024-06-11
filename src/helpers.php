b36cd6d7e427165237b0542d07a99820105e3933
Brandon <brandon@tendency.me>
2024-06-11 13:28:42 -0500

    🔧 Pass `false` to `Phar::running()`

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ if (! function_exists('laracord_path')) {
     function laracord_path(string $path = '', bool $useBasePath = true): string
     {
         $basePath = $useBasePath ? '.laracord' : '';
-        $appPath = \Phar::running() ? pathinfo(\Phar::running(), PATHINFO_DIRNAME) : null;
+        $appPath = \Phar::running(false) ? pathinfo(\Phar::running(false), PATHINFO_DIRNAME) : null;
 
         return match ($appPath) {
             null => base_path($basePath ? "{$basePath}/{$path}" : $path),
