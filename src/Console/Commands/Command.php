576f5598e8d93690c675e996a416a4a1cd462fe8
Brandon <brandon@tendency.me>
2025-02-06 06:13:08 -0600

    🎨 Remove unnecessary `WithLog` trait from `Console\Commands\Command`

--- src/Console/Commands/Command.php
+++ src/Console/Commands/Command.php
@@ -2,10 +2,9 @@
 
 namespace Laracord\Console\Commands;
 
-use Laracord\Console\Concerns\WithLog;
 use LaravelZero\Framework\Commands\Command as LaravelCommand;
 
 abstract class Command extends LaravelCommand
 {
-    use WithLog;
+    //
 }
