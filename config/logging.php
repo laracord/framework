2a2dc36ef2e5b4979e7c7cea731beff27c3506c6
Brandon <brandon@tendency.me>
2025-02-08 00:04:39 -0600

    🔧 Windows sucks

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', env('LOG_STACK', 'react')),
+            'channels' => explode(',', env('LOG_STACK', windows_os() ? 'single' : 'react')),
             'ignore_exceptions' => false,
         ],
 
